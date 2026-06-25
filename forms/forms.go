5fcf55fc4193b3dd20f60d660a578ddd0a3971b2
arthurom254 <okumuarthur69@gmail.com>
2026-06-25 12:42:18 +0300

    feat: Rename module tongforms to forms and add the form

---
+++ forms/forms.go
@@ -0,0 +1,31 @@
+package forms
+
+import "fmt"
+
+type Form struct {
+	Fields []Widget
+	Errors map[string]any
+	Data   *any
+}
+
+func (f Form) AsP() string {
+	return "<p></p>"
+}
+
+func (f Form) AsDiv() string {
+	return "<div></div>"
+}
+
+func (f Form) isBound() bool {
+	return f.Data == nil
+}
+
+func (f *Form) Clean() error {
+	if !f.isBound() {
+		return nil
+	}
+	for i := 0; i < len(f.Fields); i++ {
+		fmt.Println("Validated Form")
+	}
+	return nil
+}
---
+++ forms/widgets.go
@@ -0,0 +1,56 @@
+package forms
+
+import (
+	"fmt"
+
+	"github.com/flosch/pongo2/v6"
+)
+
+type Widget interface {
+	getContext() map[string]any
+	render() string
+}
+
+type TextField struct {
+	FieldType  string         `json:"type"`
+	FieldName  string         `json:"name"`
+	FieldValue any            `json:"value"`
+	FieldAttrs map[string]any `json:"attr"`
+}
+
+func (t TextField) getContext() map[string]any {
+
+	ctx := map[string]any{
+		"type":  t.FieldType,
+		"name":  t.FieldName,
+		"value": t.FieldValue,
+		"attrs": t.FieldAttrs,
+	}
+
+	return ctx
+}
+
+func (t TextField) render() string {
+	tpl, err := pongo2.FromFile("forms/widgets/input.html")
+	if err != nil {
+		panic(err)
+	}
+	ctx := t.getContext()
+	fmt.Println(ctx)
+	uhtml, err := tpl.Execute(pongo2.Context{
+		"widget": ctx,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	return uhtml
+}
+
+func (t TextField) String() string {
+	return t.render()
+}
+
+func Render(w Widget) string {
+	return w.render()
+}
